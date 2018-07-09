ca2ec898dca324f53904a27b092ffa24ce1069e3
Amo Moloko <u14284783@tuks.co.za>
2018-07-09 15:48:23 +0200

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+
+    entry: './app.js'
+};
